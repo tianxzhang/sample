8ed355c8c65206346aff0411f138b82904b34eff
tianxzhang <tianxzhang@163.com>
2019-06-19 09:17:24 +0000

    Finish static page

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title','Sample') --- Welcome!</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.defualt')
+@section('title','help')
+@section('content')
+    <h1>help</h1>
+@stop
