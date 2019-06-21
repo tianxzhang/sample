0dd24b855ffb32e72ce92de279cf58879a4e14cd
tianxzhang <tianxzhang@163.com>
2019-06-21 11:06:36 +0000

    Finish user crud

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -9,6 +9,13 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         return view('sessions.create');
@@ -23,7 +30,7 @@ class SessionsController extends Controller
 
         if (Auth::attempt($credentials, $request->has('remember'))) {
             session()->flash('success','Welcome!');
-            return redirect()->route('users.show',[Auth::user()]);
+            return redirect()->intended(route('users.show',[Auth::user()]));
         } else {
             session()->flash('danger','Sorry');
             return redirect()->back();
